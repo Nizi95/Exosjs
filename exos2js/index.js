db235d64fe75097e0869970353e17ec0e54d04a3
Lamouri <nlamouri@MacBook-Pro-de-Lamouri.local>
2020-09-23 16:48:26 +0200

    feat(condition): add exercice js

---
+++ exos2js/index.js
@@ -0,0 +1,63 @@
+'used strict';
+
+/**
+ * EXERCICE 1
+ */
+
+/** var age = prompt('Quel age tu as ?');
+    if (age <= 17){
+        alert('Vous ne pouvez pas entrez vous n’êtes pas majeur vous avez '+ age +' ans');   
+ *   } else if (age >=18) {
+        alert('Vous etes majeur');
+    };
+  
+  */
+    
+/**
+ * EXERCICE 2
+ */
+
+/**  var rand = Math.floor(Math.random() * (30)) + 1;
+    if (rand <= 10){
+        alert('Cool');
+    } else if (rand >=11 || rand <= 20){
+  *      alert('Tipid');
+    } if (rand >20 || rand <= 30){
+        alert('Warm');
+    }
+
+  */
+
+
+ /**
+ * EXERCICE 3
+ */ 
+
+/**
+
+var date = new Date().getDay();
+
+switch (date){
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    alert('Nous sommes '+'Monday')
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+*/
