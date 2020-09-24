5fc36d06072a4ba4998dce08bae55040c5e6996e
Lamouri <nlamouri@MacBook-Pro-de-Lamouri.local>
2020-09-24 14:19:40 +0200

    feat(exosjs): mise à jour

---
+++ conditions/conditions.js
@@ -0,0 +1,79 @@
+'used strict';
+
+/**
+ * EXERCICE 1
+ */
+
+/** var age = prompt('Quel age tu as ?');
+    if (age <= 17){
+        alert('Vous ne pouvez pas entrez vous n’êtes pas majeur vous avez '+ age +' ans');   
+ *   } else if (age >=18) {
+        alert('Vous etes majeur');
+    };
+  
+  */
+    
+/**
+ * EXERCICE 2
+ */
+
+/**  
+
+  var rand=Math.floor((Math.random() * (30) + 1));
+if (rand >= 0 && rand <= 10) {
+     alert("cool");
+} else if (rand >= 11 && rand <= 20) {
+    alert("Tiped");
+} else {
+    alert("warm")
+}
+
+  */
+
+
+ /**
+ * EXERCICE 3
+ */ 
+
+/**
+
+var date = new Date();
+var getDay = date.getDay();
+switch(getDay){
+  case 1 :
+    console.log("Il est Lundi...");
+    break;
+  case 2 :
+    console.log("Il est mardi");
+    break;
+  case 3 :
+    console.log("Il est mercredi");
+    break;
+  case 4 :
+    console.log("Il est jeudi");
+  case 5 :
+    console.log("Il est vendredi");
+  case 6 :
+    console.log("Il est samedi");
+    break;
+  case 0 :
+    console.log("Enfin dimanche !");  
+    break;
+  default:
+      console.log("jesaispas");
+}
+
+*/
+
+
+// var ternaire = ternaire != undefined ? "cette variable n'existe pas" : 42;
+
+\
+
+
+
+
+
+
+
+
