bac2b719f07f9b0c21a7461bd9a505c6bbc7e708
Nizi95 <nlamouri@MacBook-Pro-de-Lamouri.local>
2020-09-24 19:18:44 +0200

    feat(exojs): add exercice to function

---
+++ function/function.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+    Exercice 1
+  */
+
+ /** var myPutStr = function myPutStr(value){
+      if (typeof value === 'string'){
+          return "c'est un string"
+      }
+
+      if(typeof value === 'number'){
+          return "et pourquoi pas 42 ?"
+      }
+  }
+ 
+     myPutStr(50); */
+/**
+   Exercice 2 
+  */
+
+   var computeSurfaceM2 = function computeSurfaceM2(width, length){
+          return width * length;
+   }
+
+    computeSurfaceM2(2,4);
+
+/**
+   Exercice 3 
+  */
